e7a616cc3ac4f2d557f285d5ee3fd0cd3c5612bb
Dans182 <dgaiteiro@gmail.com>
2022-03-31 18:56:09 +0000

    css y bootstrap listo

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SecondsCounter = () => {
+	return (
+		<div className="col">
+			<div
+				className="card text-white bg-dark m-1"
+				style={{ maxWidth: "18rem" }}>
+				<div className="card-body text-center">
+					<h1 className="card-title">0</h1>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SecondsCounter;
